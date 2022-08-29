52d22206dca6eecbaaf14464f16b0da029f5a80f
Melyn Atieno Were <melynatieno@gmail.com>
2022-08-29 11:11:59 +0300

    commit tree 489193ddfb8520215a7fed3d9792474068063455

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 27, "M", "Brown", "Phlegmatic", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 16, "M", "Blue", "Melancholic", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 36, "F", "Black", "Phlegmatic", 1);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 29, "M", "White", "Choleric", 0);
+INSERT INTO bears VALUES (5,"Melissa", 33, "F", "Brown", "Sanguine", 1);
+INSERT INTO bears VALUES (6,"Grinch", 22, "M", "Purple", "Melancholic", 0);
+INSERT INTO bears VALUES (7,"Wendy", 32, "F", "Black", "Phlegmatic", 1);
+INSERT INTO bears VALUES (8,null, 25, "M", "Blue", "Choleric", 0);
